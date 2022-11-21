79a683dd36a963c03bb392d7df85c45db8f07c09
Momo Coul <mc9915118@gmail.com>
2022-11-21 16:52:45 +0100

    yyjh

---
+++ age.py
@@ -0,0 +1,2 @@
+age=int(input("Quel age avez-vous: "))
+print(f"votre année de naissance est : ",2022- age )
---
+++ test.py
@@ -0,0 +1,7 @@
+
+x=int(input("donner x: "))
+y=int(input("donner y: "))
+a=x
+x=y
+y=a
+print (f"apres permutation X:{x} et Y: {y} ")
